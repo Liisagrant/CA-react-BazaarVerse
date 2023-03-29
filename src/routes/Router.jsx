393d59429629a7a6ba005e4f56f59a7f7ea112b5
Liisagrant <lisagrant1990@gmail.com>
2023-03-29 10:55:45 +0200

    Refactored landing page to be more clear and understandable, renaming it to "Homepage". Created a Homepage component containing a hero banner and a section displaying all products.

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AllProducts from "../components/pages/ProductsPage/AllProducts";
 import ProductDetails from "../components/pages/ProductDetailPage/ProductDetail";
-import Herobanner from "../components/pages/HomePage/HeroBanner";
+import HomePage from "../components/pages/HomePage/HomePage";
 
 function Router() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<AllProducts />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<AllProducts />} />
         <Route path="/products/:id" element={<ProductDetails />} />
       </Routes>
