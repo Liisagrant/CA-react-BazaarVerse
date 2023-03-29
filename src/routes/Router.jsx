632c66b2c84f40f9052bf680b7802a55a7da1c17
Liisagrant <lisagrant1990@gmail.com>
2023-03-29 11:58:57 +0200

    created a ShoppingCart page, added it to router and fixed hover issues in the header for the cart icon

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import AllProducts from "../components/pages/ProductsPage/AllProducts";
 import ProductDetails from "../components/pages/ProductDetailPage/ProductDetail";
 import HomePage from "../components/pages/HomePage/HomePage";
+import ShoppingCart from "../components/pages/CartPage/ShoppingCart";
 
 function Router() {
   return (
@@ -11,6 +12,7 @@ function Router() {
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<AllProducts />} />
         <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<ShoppingCart />} />
       </Routes>
     </>
   );
