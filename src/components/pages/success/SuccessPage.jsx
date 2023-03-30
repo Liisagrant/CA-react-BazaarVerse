c118ceb2b589208c5b9543a511c8e15d7362c074
Liisagrant <lisagrant1990@gmail.com>
2023-03-30 20:31:54 +0200

    added styling, image and linking for success page

--- src/components/pages/success/SuccessPage.jsx
+++ src/components/pages/success/SuccessPage.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import successImg from "../../../../public/assets/success.jpg";
 
 const SuccessPage = () => {
-  return <div> yaaay</div>;
+  return (
+    <div className="grid grid-cols-1 grid-rows-[1fr,auto,1fr] bg-white lg:grid-cols-[max(50%,36rem),1fr]">
+      <main className="mx-auto w-full max-w-7xl px-6 py-24 sm:py-32 lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:px-8">
+        <div className="max-w-lg">
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Thank you for your order! Your purchase was successful.
+          </h1>
+          <p className="mt-6 text-base leading-7 text-gray-600">
+            We're processing your order now. Estimated delivery time is 5-7
+            business days. You'll receive a confirmation email with your order
+            details and tracking number shortly. We appreciate your business and
+            hope you enjoy your purchase!
+          </p>
+          <div className="mt-10">
+            <Link
+              to="/"
+              className="text-sm font-semibold leading-7 text-lightblue underline "
+            >
+              <span aria-hidden="true">&larr;</span> Back to home
+            </Link>
+          </div>
+          <div className="mt-10">
+            <Link
+              to="/products"
+              className="text-sm font-semibold leading-7 text-lightblue underline"
+            >
+              <span aria-hidden="true">&larr;</span> Or some more shopping?
+            </Link>
+          </div>
+        </div>
+      </main>
+      <div className="hidden lg:relative lg:col-start-2 lg:row-start-1 lg:row-end-4 lg:block">
+        <img
+          src={successImg}
+          alt="Thank you image"
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SuccessPage;
