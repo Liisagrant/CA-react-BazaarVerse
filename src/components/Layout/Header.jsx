81c33a9678cf1bdebcb9fb8fdc53cd10fa6036d3
Liisagrant <lisagrant1990@gmail.com>
2023-03-31 14:53:19 +0200

    added a error handler for homepage. Also removed no use code and removed consol.logs

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useSelector } from "react-redux";
 import { NavLink, Link } from "react-router-dom";
 import LOGO from "../../../public/assets/BazzarVerseLOGO.svg";
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "@reduxjs/toolkit";
 import productsSlice from "./modules/productSlice";
 import cartSlice from "./modules/cartSlice";
+import errorslice from "./modules/errorSlice";
 
 const reducer = combineReducers({
   products: productsSlice,
   cart: cartSlice,
+  error: errorslice,
 });
 const index = configureStore({
   reducer,
