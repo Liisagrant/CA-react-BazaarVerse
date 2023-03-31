8b06aec268f9bf438a698741f92af0bcc929435b
Liisagrant <lisagrant1990@gmail.com>
2023-03-31 21:07:39 +0200

    Created global loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import Layout from "./components/Layout/Layout";
 import Router from "./routes/Router";
+import { useSelector } from "react-redux";
+import SpinnerComponent from "./components/global/SpinnerComponent";
 
 function App() {
+  const { isLoading } = useSelector((state) => state.loader);
   return (
     <>
       <BrowserRouter>
         <Layout>
           <Router />
+          {isLoading && <SpinnerComponent />}
         </Layout>
       </BrowserRouter>
     </>
--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from "@reduxjs/toolkit";
 import productsSlice from "./modules/productSlice";
 import cartSlice from "./modules/cartSlice";
 import errorslice from "./modules/errorSlice";
+import loaderSlice from "./modules/loaderSlice";
 
 const reducer = combineReducers({
   products: productsSlice,
   cart: cartSlice,
   error: errorslice,
+  loader: loaderSlice,
 });
 const index = configureStore({
   reducer,
